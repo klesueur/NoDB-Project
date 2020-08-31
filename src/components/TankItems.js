47d37653eb3bc3088a10b72a8c39d0bb0f58bb22
Kara LeSueur <kara.lesueur@gmail.com>
2020-08-30 18:21:42 -0700

    restart front end

---
+++ src/components/TankItems.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const TankItems = (props) => {
+    return (
+
+        <div className="tank-item">
+            <div>
+                <div className="tankPlantButton">
+                    <button>-</button>
+                    <button>+</button>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default TankItems
